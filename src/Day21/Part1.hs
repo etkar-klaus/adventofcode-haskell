2472948b15ac8982ab6c173706c0a1bccb7c68ad
daniel.beer <etkar.klaus@googlemail.com>
2022-01-07 23:35:55 +0100

    Day 21

--- src/Day21/Part1.hs
+++ src/Day21/Part1.hs
@@ -9,6 +9,9 @@ data State
       {- player1 :: -} Player
       {- player2 :: -} Player
 
+maxScore :: Int
+maxScore = 1000
+
 solve :: [String] -> Integer
 solve = result . until end next . initState . map parse
 
@@ -16,19 +19,19 @@ parse :: String -> Int
 parse = read . drop 28
 
 initState :: [Int] -> State
-initState (pos1 : pos2 : _) = State (cycle [1 .. 100]) 0 (Player pos1 0) (Player pos2 0)
-initState _ = error "Too few elements"
+initState [pos1, pos2] = State (cycle [1 .. 100]) 0 (Player pos1 0) (Player pos2 0)
+initState _ = error "Wrong number of elements"
 
 end :: State -> Bool
-end (State _ _ _ (Player _ score2)) = score2 >= 1000
+end (State _ _ _ (Player _ score2)) = score2 >= maxScore
 
 next :: State -> State
 next (State dice rolls pl1 pl2) = State (drop 3 dice) (rolls + 3) pl2 (pl1 `move` take 3 dice)
 
 move :: Player -> [Int] -> Player
-move (Player pos score) dice = Player newPos (score + newPos)
-  where
-    newPos = (pos + sum dice - 1) `mod` 10 + 1
+move (Player pos score) dice =
+  let newPos = (pos + sum dice - 1) `mod` 10 + 1
+   in Player newPos (score + newPos)
 
 result :: State -> Integer
 result (State _ rolls (Player _ score1) _) = toInteger score1 * toInteger rolls
--- src/Day21/Part2.hs
+++ src/Day21/Part2.hs
@@ -1,4 +1,65 @@
-module Day21.Part2 where
+module Day21.Part2 (solve) where
+
+import Data.Map (fromList, lookup)
+import Data.Maybe (fromMaybe)
+import Prelude hiding (lookup)
+
+data Wins = Wins Integer Integer
+
+data Player = Player {- position :: -} Int {- score :: -} Int
+  deriving (Eq, Ord)
+
+data State = State Player Player
+  deriving (Eq, Ord)
+
+maxScore :: Int
+maxScore = 21
 
 solve :: [String] -> Integer
-solve = error "Not yet implemented"
+solve = result . move . initState . map parse
+
+parse :: String -> Int
+parse = read . drop 28
+
+initState :: [Int] -> State
+initState [pos1, pos2] = State (Player pos1 0) (Player pos2 0)
+initState _ = error "Wrong number of elements"
+
+roll :: State -> Int -> Wins
+roll (State (Player pos score) player2) die =
+  let newPos = (pos + die - 1) `mod` 10 + 1
+      newScore = score + newPos
+   in if newScore >= maxScore
+        then Wins 1 0
+        else swap $ memoizedMove $ State player2 (Player newPos newScore)
+
+move :: State -> Wins
+move state =
+  let f = mul . roll state
+   in foldl add (Wins 0 0) [f 3 1, f 4 3, f 5 6, f 6 7, f 7 6, f 8 3, f 9 1]
+
+memoizedMove :: State -> Wins
+memoizedMove state =
+  fromMaybe (error "not found") $
+    lookup
+      state
+      ( fromList $
+          [ let s = State (Player p1 s1) (Player p2 s2) in (s, move s)
+            | p1 <- [1 .. 10],
+              p2 <- [1 .. 10],
+              s1 <- [0 .. maxScore],
+              s2 <- [0 .. maxScore]
+          ]
+      )
+
+swap :: Wins -> Wins
+swap (Wins win1 win2) = Wins win2 win1
+
+add :: Wins -> Wins -> Wins
+add (Wins win11 win12) (Wins win21 win22) = Wins (win11 + win21) (win12 + win22)
+
+mul :: Wins -> Integer -> Wins
+mul (Wins win1 win2) weight = Wins (win1 * weight) (win2 * weight)
+
+result :: Wins -> Integer
+result (Wins win1 win2) = max win1 win2
