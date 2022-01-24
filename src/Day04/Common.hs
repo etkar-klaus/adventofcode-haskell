ddbca65698c9b8ab1c1f4814a14c96ff9021a4a2
daniel.beer <etkar.klaus@googlemail.com>
2022-01-24 08:29:18 +0100

    More code reduction

--- src/Day04/Common.hs
+++ src/Day04/Common.hs
@@ -1,6 +1,5 @@
 module Day04.Common (finished, valueOf, markRepeat) where
 
-import Control.Applicative (Applicative (liftA2))
 import Data.Array (Array, elems, listArray, (!), (//))
 import Data.List (elemIndex)
 import Data.List.Split (chunksOf, splitOn)
@@ -12,7 +11,7 @@ valueOf :: ([Bingo] -> Bingo) -> [String] -> Integer
 valueOf select = toInteger . value . select . play
 
 play :: [String] -> [Bingo]
-play input = markRepeat (parseNumbers $ head input) (parseBingos input)
+play input = concat $ markRepeat (parseBingos input) (parseNumbers $ head input)
 
 bingo :: [[Int]] -> Bingo
 bingo nums = Bingo (listArray (0, 24) $ repeat False) (concat nums) (-1)
@@ -29,22 +28,18 @@ parseCells = map read . words
 parseNumbers :: String -> [Int]
 parseNumbers = map read . splitOn [',']
 
-markRepeat :: [Int] -> [Bingo] -> [Bingo]
-markRepeat [] = id
-markRepeat (n : nums) = liftA2 (++) id (markRepeat nums . markAll n . filter (not . finished))
-
-markAll :: Int -> [Bingo] -> [Bingo]
-markAll = map . markMaybe
+markRepeat :: [Bingo] -> [Int] -> [[Bingo]]
+markRepeat = scanl $ flip (map . markMaybe) . filter (not . finished)
 
 markMaybe :: Int -> Bingo -> Bingo
 markMaybe num (Bingo marks numbers _) = maybe Bingo setMark (elemIndex num numbers) marks numbers num
 
-setMark :: Int -> Array Int Bool -> [Int] -> (Int -> Bingo)
+setMark :: Int -> Array Int Bool -> [Int] -> Int -> Bingo
 setMark index marks = Bingo (marks // [(index, True)])
 
 finished :: Bingo -> Bool
--- finished = liftA2 (||) (flip any [0 .. 4] . horizontal) (flip any [0 .. 4] . vertical)
-finished b = any (horizontal b) [0 .. 4] || any (vertical b) [0 .. 4]
+-- finished b = any (horizontal b) [0 .. 4] || any (vertical b) [0 .. 4]
+finished b = or $ [horizontal b, vertical b] <*> [0 .. 4]
 
 horizontal :: Bingo -> Int -> Bool
 horizontal (Bingo marks _ _) row = all (marks !) [row * 5 + col | col <- [0 .. 4]]
@@ -56,7 +51,4 @@ value :: Bingo -> Int
 value (Bingo marks numbers lastNum) = condSum (elems marks) numbers * lastNum
 
 condSum :: [Bool] -> [Int] -> Int
-condSum _ [] = 0
-condSum [] _ = 0
-condSum (False : marks) (x : xs) = x + condSum marks xs
-condSum (True : marks) (_ : xs) = condSum marks xs
+condSum marks nums = sum $ zipWith (\m n -> if m then 0 else n) marks nums
